9261ce18816df0de95be938d280b6d6cc47544be
Parviz <p.ahmadov@student.tudelft.nl>
2025-07-22 20:04:17 +0200

    yelp2018 model improved

--- recbole/model/sequential_recommender/sasrec_sae.py
+++ recbole/model/sequential_recommender/sasrec_sae.py
@@ -39,15 +39,14 @@ class SASRec_SAE(SASRec):
         )
         position_ids = position_ids.unsqueeze(0).expand_as(item_seq)
         position_embedding = self.position_embedding(position_ids)
-		
+
 		# Reconstruct the embedding weights using SAE
         reconstructed_weights = self.sae_module_i(self.item_embedding.weight)
         # Use reconstructed weights for embedding lookup
         item_emb = torch.nn.functional.embedding(item_seq, reconstructed_weights, padding_idx=0)
 
 
-        item_emb_sae = self.sae_module_i(item_emb, train_mode=train_mode)
-        input_emb = item_emb_sae + position_embedding
+        input_emb = item_emb + position_embedding
         input_emb = self.LayerNorm(input_emb)
         input_emb = self.dropout(input_emb)
 
